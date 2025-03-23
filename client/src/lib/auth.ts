d8f21de9741b8d6ca37f9d534d0302b8ebdede35
David Udoh <udohdavid004@gmail.com>
2025-03-23 12:34:57 +0100

    fixed session

--- client/src/lib/auth.ts
+++ client/src/lib/auth.ts
@@ -1,61 +1,32 @@
 import { API_URL } from "@/config";
-import type { User } from "@chessu/types";
-import axios from "axios";
-import { clientErrorHandler } from "./handlers";
+import { authApi } from "./api";
 
-export const fetchSession = () =>
-    clientErrorHandler(async () => {
-        const { data } = await axios.get(`${API_URL}/v1/auth/getuser`);
-        return data;
-    });
+export const fetchSession = () => authApi.get(`${API_URL}/v1/auth`);
 
-export const login = (name: string, password: string) =>
-    clientErrorHandler(async () => {
-        const { data } = await axios.post(`${API_URL}/v1/auth/login`, { name, password });
-        return data;
-    });
+export const register = async (name: string, password: string, email?: string) =>
+    authApi.post(`${API_URL}/v1/auth/register`, { name, password, email });
 
-export const register = (name: string, password: string, email: string) =>
-    clientErrorHandler(async () => {
-        const { data } = await axios.post(`${API_URL}/v1/auth/register`, { name, password, email });
-        return data;
-    });
+export const login = async (name: string, password: string) =>
+    authApi.post(`${API_URL}/v1/auth/login`, { name, password });
 
-export const updateUser = (name?: string, email?: string, password?: string) =>
-    clientErrorHandler(async () => {
-        const { data } = await axios.patch(`${API_URL}/v1/auth/`, { name, email, password });
-        return data;
-    });
+export const updateUser = (name?: string, email?: string, password?: string) => {
+    if (!name && !email && !password) return;
+    authApi.patch(`${API_URL}/v1/auth/`, { name, email, password });
+};
 
-export const logout = () =>
-    clientErrorHandler(async () => {
-        const { data } = await axios.post(`${API_URL}/v1/auth/logout`);
-        return data;
-    });
+export const logout = () => authApi.post(`${API_URL}/v1/auth/logout`, {});
 
 export const resendMail = (email: string) =>
-    clientErrorHandler(async () => {
-        const { data } = await axios.post(`${API_URL}/v1/auth/resendmail`, { email });
-        return data;
-    });
+    authApi.post(`${API_URL}/v1/auth/resendmail`, { email });
 
 export const verifyMail = (token: string) =>
-    clientErrorHandler(async () => {
-        const { data } = await axios.post(`${API_URL}/v1/auth/verifymail`, { token });
-        return data;
-    });
+    authApi.post(`${API_URL}/v1/auth/verifymail`, { token });
 
 export const sendForgotPassMail = (email: string, password: string) =>
-    clientErrorHandler(async () => {
-        const { data } = await axios.post(`${API_URL}/v1/auth/forgotpassmailsend`, {
-            email,
-            password
-        });
-        return data;
+    authApi.post(`${API_URL}/v1/auth/forgotpassmailsend`, {
+        email,
+        password
     });
 
 export const verifyForgotPassMail = (token: string) =>
-    clientErrorHandler(async () => {
-        const { data } = await axios.post(`${API_URL}/v1/auth/forgotpassmailverify`, { token });
-        return data;
-    });
+    authApi.post(`${API_URL}/v1/auth/forgotpassmailverify`, { token });
